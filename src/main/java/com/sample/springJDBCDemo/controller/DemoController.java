6c24238fec34da3f320b7fe34acd43d4dd68641e
Jayesh2024 <jayeshsavle24@gmail.com>
2024-05-13 01:16:42 +0530

    write some unit test cases

---
+++ src/main/java/com/sample/springJDBCDemo/controller/DemoController.java
@@ -0,0 +1,56 @@
+package com.sample.springJDBCDemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.springJDBCDemo.model.User;
+import com.sample.springJDBCDemo.service.DemoService;
+
+@RestController
+public class DemoController {
+    
+    @Autowired
+    DemoService demoService;
+
+    @PostMapping("/save")
+    public User saveUser(@RequestBody User user){
+        User userSaved = demoService.saveUser(user);
+        return userSaved;
+    }
+
+    public List<User> getAllUser(){
+        List<User> userList = demoService.getAllUser();
+        return userList;
+    }
+
+    @GetMapping("/getUser-byId/{id}")
+    public User getUserById(@PathVariable int id) {
+        return demoService.getUserById(id);
+    }
+    
+    @PutMapping("update-user/{id}")
+    public User updateUser(@PathVariable int id, @RequestParam("name") String name, @RequestParam("city") String city) {
+        User updatedUser = new User(id, name, city);
+        demoService.updateUser(updatedUser);
+        return updatedUser;
+    }
+
+    @DeleteMapping("/delete-user")
+    public void deleteUser(@RequestParam("id") int id){
+        demoService.deleteUserById(id);
+    }
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/sample/springJDBCDemo/service/DemoService.java
@@ -0,0 +1,38 @@
+package com.sample.springJDBCDemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sample.springJDBCDemo.Repository.DemoRepository;
+import com.sample.springJDBCDemo.model.User;
+
+@Service
+public class DemoService {
+
+    @Autowired
+    DemoRepository demoRepository;
+    
+    public User saveUser(User user) {
+        return demoRepository.saveUser(user);
+    }
+
+    public User getUserById(int id){
+        return demoRepository.getUserById(id);
+    }
+
+    public List<User> getAllUser(){
+        List<User> users = demoRepository.getAllUsers();
+        return users;
+    }
+
+    public User updateUser(User user){
+        return demoRepository.updateUser(user);
+    }
+
+    public void deleteUserById(int id){
+        demoRepository.deleted(id);
+    }
+
+}
